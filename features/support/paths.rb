7917938d8886aac5e410a5a3804885c54a5c9761
sungil lim <chamon1231@hotmail.com>
2012-11-21 19:47:37 +0800

    Addedtheabilityto view a list of all projects

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,23 +1,33 @@
 module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
   def path_to(page_name)
     case page_name
-    
-    when /the home\s?page/
-      '/'  
-    # Add more page name => path mappings here
-    else
-      if path = match_rails_path_for(page_name) 
-        path
-      else 
-        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in features/support/paths.rb"
-      end
-    end
-  end
 
-  def match_rails_path_for(page_name)
-    if page_name.match(/the (.*) page/)
-      return send "#{$1.gsub(" ", "_")}_path" rescue nil
+    when /the home\s?page/
+      '/'
+
+    when /the project page for "([^\"]*)"/
+      project_path(Project.find_by_name!($1))
+    # Add more mappings here.
+    # Here is an example that pulls values out of the Regexp:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
+
+    else
+      begin
+        page_name =~ /the (.*) page/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue Object => e
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
     end
   end
 end
